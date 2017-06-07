0f748b682f87852fd6d92c146725709a9a64a842
Christophe Dirac <cdirac@me.com>
2017-06-07 19:43:42 +0200

    Initial commit

---
+++ src/EspAtmegaCom.cpp
@@ -0,0 +1,54 @@
+#include "EspAtmegaCom.h"
+
+EspAtmegaCom::EspAtmegaCom(HardwareSerial &serial) {
+	this->_serial = &serial;
+	setCallback(NULL);
+}
+
+EspAtmegaCom::EspAtmegaCom(HardwareSerial &serial, EAC_CALLBACK_SIGNATURE) {
+	this->_serial = &serial;
+	setCallback(callback);
+}
+
+String EspAtmegaCom::read() {
+  if (_serial->available()) {
+    String data = _serial->readStringUntil('\n');
+    data.trim();
+    int startTag = data.indexOf(EAC_START_TAG);
+    int endTag = data.indexOf(EAC_END_TAG);
+    if (startTag >= 0 && endTag >= 0) {
+      data.remove(endTag);
+      data.remove(0, startTag + 3);
+      return data;
+    }
+    return "";
+  }
+  return "";
+}
+
+int EspAtmegaCom::available() {
+	return _serial->available();
+}
+
+void EspAtmegaCom::begin() {
+	_serial->begin(115200);
+	while (!_serial) {
+		// wait serial port initialization
+	}
+	_serial->setTimeout(100);
+}
+
+void EspAtmegaCom::send(String payload) {
+	_serial->println(EAC_START_TAG + payload + EAC_END_TAG);
+}
+
+void EspAtmegaCom::loop() {
+	String data = read();
+	if (data.length() > 0 && callback) {
+		callback(data);
+	}
+}
+
+void EspAtmegaCom::setCallback(EAC_CALLBACK_SIGNATURE) {
+	this->callback = callback;
+}
---
+++ src/EspAtmegaCom.h
@@ -0,0 +1,33 @@
+#ifndef EspAtmegaCom_h
+#define EspAtmegaCom_h
+
+#include <WString.h>
+#include <HardwareSerial.h>
+
+#define EAC_START_TAG "+S+"
+#define EAC_END_TAG "-E-"
+
+#ifdef ESP8266
+#include <functional>
+#define EAC_CALLBACK_SIGNATURE std::function<void(String)> callback
+#else
+#define EAC_CALLBACK_SIGNATURE void (*callback)(String)
+#endif
+
+class EspAtmegaCom {
+  private:
+	HardwareSerial *_serial;
+	EAC_CALLBACK_SIGNATURE;
+	String read();
+	int available();
+  public:
+	EspAtmegaCom(HardwareSerial&);
+	EspAtmegaCom(HardwareSerial&,EAC_CALLBACK_SIGNATURE);
+	void begin();
+	void send(String);
+	void loop();
+	void setCallback(EAC_CALLBACK_SIGNATURE);
+};
+
+#endif
+
